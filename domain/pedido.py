5c62d2ee444656ccbcb3c8184faa5fa28422f01d
AlunoTI <AlunoTI@estacio.acad>
2026-04-06 20:52:54 -0300

    api lanchonete

---
+++ domain/pedido.py
@@ -0,0 +1,38 @@
+from typing import List
+from domain.cliente import Cliente
+from domain.produto import Produto
+
+class Pedido:
+    _seq = 1 # autoincremento (estático)
+    def __init__(self, cliente: Cliente, qtd_max_produtos: int):
+        self._codigo = Pedido._seq
+        Pedido._seq += 1
+        self.cliente = cliente
+        self.qtd_max_produtos = int(qtd_max_produtos)
+        self.listaProdutos: List[Produto] = []
+        self.estaEntregue: bool = False
+ 
+    @property
+    def codigo(self) -> int:
+        return self._codigo # imutável
+ 
+    def adicionar_produto(self, produto: Produto) -> bool:
+        if len(self.listaProdutos) >= self.qtd_max_produtos:
+            return False
+        self.listaProdutos.append(produto)
+        return True
+ 
+    def finalizar(self) -> float:
+        self.estaEntregue = True
+        total = 0.0
+        for p in self.listaProdutos:
+            total += p.preco_final()
+        return float(total)
+    
+    def total_se_finalizado(self) -> float:
+        if not self.estaEntregue:
+            return 0.0
+        total = 0.0
+        for p in self.listaProdutos:
+            total += p.preco_final()
+        return float(total)
